c138c541446e8189922ab974e728e0db2add7a35
ashraf733 <110895326+ashraf733@users.noreply.github.com>
2022-08-09 14:21:49 +0530

    Add files via upload

---
+++ src/conn.java
@@ -0,0 +1,20 @@
+import java.sql.*;
+
+public class conn
+{
+    Connection c;
+    Statement s;
+    public conn()
+    {
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+            c=DriverManager.getConnection("jdbc:mysql://localhost:3306/electri","root","ashraf3@A");
+            s=c.createStatement();
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+    }
+}
